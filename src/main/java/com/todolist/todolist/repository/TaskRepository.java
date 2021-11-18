bb75f4d9d49db88ec2d81b64ead29e9509ab0609
ruanvalente <ruanopensuselinux@gmail.com>
2021-11-18 07:53:51 -0300

    feature: add task repository

--- src/main/java/com/todolist/todolist/repository/TaskRepository.java
+++ src/main/java/com/todolist/todolist/repository/TaskRepository.java
@@ -1,5 +1,8 @@
 package com.todolist.todolist.repository;
 
-public interface TaskRepository {
-    
-}
+import com.todolist.todolist.model.Task;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TaskRepository extends JpaRepository<Task, Long> { }
